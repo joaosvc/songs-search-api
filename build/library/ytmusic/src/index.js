b61400ad25f54d51360cd425a23cb468d63e4567
Joaosvc <joaosvc.dev@gmail.com>
2024-04-19 23:34:19 -0300

    feat: Removing build folder

--- build/library/ytmusic/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const YTMusic_1 = tslib_1.__importDefault(require("./YTMusic"));
-exports.default = YTMusic_1.default;
--- build/utils/search-options.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SEARCH_OPTIONS = exports.ISRC_REGEX = void 0;
-exports.ISRC_REGEX = /^[A-Z]{2}-?\w{3}-?\d{2}-?\d{5}$/;
-exports.SEARCH_OPTIONS = [
-    { filter: "songs", limit: 50 },
-    { filter: "videos", limit: 50 },
-];
